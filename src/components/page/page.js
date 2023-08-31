640a8d6794fe920140b4cfdd1f29f19fd2344f80
kseniaagapitova <kagapitova@inbox.ru>
2023-08-31 15:46:33 +0300

    feat: start layout

---
+++ src/components/page/page.js
@@ -0,0 +1,25 @@
+function renderPage(){
+	let pageHTML = `
+<div class="main">
+    <nav class="navigation"></nav>
+    <section class="main__container">
+	    <div class="cart__container">
+	    		<div class="cart"></div>
+	        <div class="no-product__container"></div>
+	        <div class="info__container">
+	            <div class="delivery__container"></div>
+	            <div class="payment__container"></div>
+	            <div class="recipient-info__container"></div>
+	        </div>
+			</div>
+      <div class="total__container">
+	         <div class="total__card"></div>
+			</div>
+		</section>
+    <footer class="footer__container"></footer>
+</div>
+`;
+	const body = document.querySelector('body');
+	body.innerHTML = pageHTML
+	console.log('страница есть')
+}
